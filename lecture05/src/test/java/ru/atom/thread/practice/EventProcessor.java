8e956e3e773ab7bfb9ae8b57cd923465331ab5bd
alexandraaryemyeva <sandra.artemyeva@gmail.com>
2017-11-01 17:03:57 +0300

    done

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -7,15 +7,33 @@ import java.util.List;
  * @since 15.03.17
  */
 public class EventProcessor {
+    private static long good = 0, bad = 0;
+
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new UnsupportedOperationException();
+        for (EventProducer i : eventProducers) {
+            i.run();
+        }
+    }
+    private static void countAll()
+    {
+        while (!EventQueue.getInstance().isEmpty())
+        {
+            Event theEvent = EventQueue.getInstance().poll();
+            if (theEvent.getEventType() == Event.EventType.GOOD)
+                good++;
+            else bad++;
+        }
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new UnsupportedOperationException();
+        countAll();
+        return good;
+
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new UnsupportedOperationException();
+        countAll();
+        return bad;
     }
 }
